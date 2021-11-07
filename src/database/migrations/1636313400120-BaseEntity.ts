498d5e31287ca78a41d99ca32ea78de6fbe1b446
Kylian <kylian.cadet@epitech.eu>
2021-11-07 20:30:38 +0100

    chore: add migration script with createdBy and lastUpdatedBy converted to uuid 	new file:   src/database/migrations/1636313400120-BaseEntity.ts

---
+++ src/database/migrations/1636313400120-BaseEntity.ts
@@ -0,0 +1,68 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class BaseEntity1636313400120 implements MigrationInterface {
+    name = 'BaseEntity1636313400120'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "message_flux" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "message_flux" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "message_flux" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "message_flux" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "message" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "message" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "message" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "message" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "server" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "server" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "server" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "server" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "lastUpdatedBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "friend_request" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "friend_request" ADD "createdBy" uuid`);
+        await queryRunner.query(`ALTER TABLE "friend_request" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "friend_request" ADD "lastUpdatedBy" uuid`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "friend_request" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "friend_request" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "friend_request" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "friend_request" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "server" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "server" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "server" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "server" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "server_text_channel" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "user_text_channel" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "message" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "message" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "message" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "message" ADD "createdBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "message_flux" DROP COLUMN "lastUpdatedBy"`);
+        await queryRunner.query(`ALTER TABLE "message_flux" ADD "lastUpdatedBy" character varying`);
+        await queryRunner.query(`ALTER TABLE "message_flux" DROP COLUMN "createdBy"`);
+        await queryRunner.query(`ALTER TABLE "message_flux" ADD "createdBy" character varying`);
+    }
+
+}
